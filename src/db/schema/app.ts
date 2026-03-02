2ae74447a1d9a8846ab2bd8c1bc21afb2e6f0a21
EonEmmanuel <emmanueltresor027@gmail.com>
2026-03-02 17:24:28 +0100

    adding files extensions

--- src/db/schema/app.ts
+++ src/db/schema/app.ts
@@ -1,6 +1,6 @@
 import {pgTable, integer, varchar, timestamp, pgEnum, text, uniqueIndex, index, jsonb, unique} from "drizzle-orm/pg-core";
 import {relations} from "drizzle-orm";
-import {user} from "./auth";
+import {user} from "./auth.js";
 
 const timestamps = {
     createdAt: timestamp('created_at').defaultNow().notNull(),
